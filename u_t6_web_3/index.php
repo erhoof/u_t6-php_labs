43b5f53c33cd57a8f128b904706ff0d56d8c2edd
Pavel Bibichenko <erhoof@erhoofs-laptop.Dlink>
2021-04-20 09:22:09 +0300

    l3 add output format form

--- u_t6_web_3/index.php
+++ u_t6_web_3/index.php
@@ -13,10 +13,31 @@
         th, td {
             padding: 5px;
         }
+
+        label {
+            display: inline-block;
+            width: 150px;
+            text-align: right;
+        }
     </style>
 </head>
 <body>
     <h3>Таблица синусов</h3>
+    <form action="index.php" method="post">
+        <p>Укажите формат ввода до\после запятой</p>
+
+        <div>
+            <label for="before">До запятой </label>
+            <input type="number" name="before" value=5>
+        </div>
+
+        <div>
+            <label for="after">После запятой </label>
+            <input type="number" name="after" value=3>
+        </div><br>
+
+        <input type="submit" value="Перевести">
+    </form><br>
     <table>
         <tr>
             <th colspan="2">Угол</th>
@@ -27,10 +48,21 @@
             <th>В радианах</th>
         </tr>
         <?php
+            $before = $_POST["before"];
+            $after = $_POST["after"];
+
+            if ($before == null || (int)$before > 10)
+                $before = 5;
+
+            if ($after == null || (int)$after > 10)
+                $after = 5;
+
+            $format = "%{$before}.{$after}f";
+
             for ($i = 0; $i <= 90; $i += 5) {
                 $rad = deg2rad($i);
                 $sin = sin($rad);
-                $radFormat = sprintf("%5.3f", $rad);
+                $radFormat = sprintf($format, $rad);
                 $sinFormat = sprintf("%5.3f", $sin);
 
                 echo "  <tr>
